f5b625938230e2b7ea90581c2d2ac77f84b1b315
mahibmhd <mahletdemeke0791@gmail.com>
2023-06-20 15:45:28 +0300

    nav bar updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
-import {AiFillTwitterCircle, AiFillLinkedin} from 'react-icons/ai' ;
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from 'react-icons/ai' ;
 import { useState } from "react";
 import mahdev from "../public/portfolio1.jpg";
 import Image from "next/image";
@@ -22,15 +22,21 @@ export default function Home() {
       <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className=" font-burtons text-xl">developedbymahi</h1>
-         <ul className="flex items-center">
+            <h1 className=" font-burtons text-xs">developedbymahi</h1>
+            <ul className="flex pr-20">
+          <li className="font-bold text-xl px-3 hover:text-[#52c1b4] hover:border-b-4 border-white duration-300 ease-linear cursor-pointer"> Home</li>
+          <li  className="font-bold text-xl px-3 hover:text-[#52c1b4] hover:border-b-4 border-white duration-300 ease-linear cursor-pointer">About</li>
+          <li  className="font-bold text-xl px-3 hover:text-[#52c1b4] hover:border-b-4 border-white duration-300 ease-linear cursor-pointer">Services</li> 
+          <li  className="font-bold text-xl px-3 hover:text-[#52c1b4] hover:border-b-4 border-white duration-300 ease-linear cursor-pointer">Contact</li>    
+            </ul>
+         <ul className="flex items-start">
           <li>
             < BsFillMoonStarsFill 
             onClick={() => setDarkMode(!darkMode)} 
             className="cursor-pointer text-2xl" 
             /> 
           </li>
-           </ul>
+           </ul>      
           </nav>
           <div className=" text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-white md:text-3xl">
@@ -43,11 +49,12 @@ export default function Home() {
             Freelancer providing services for programming and design content needs. Join me down below and let's get cracking!
             </p>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+             <AiFillGithub />
             <AiFillTwitterCircle />
             <AiFillLinkedin />
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 overflow-hidden mt-20 ">
-          <Image src={mahdev} width={350} height={200} />
+          {/* <Image src={mahdev} width={350} height={200} /> */}
          {/* <Image 
           src={mahdev}
           style={{
@@ -77,8 +84,8 @@ export default function Home() {
               programming and teaching.
             </p>
           </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+          <div className="lg:flex gap-20">
+            <div className="text-center shadow-2xl p-10 rounded-xl my-10  dark:bg-white flex-1">
               <h3 className="text-lg font-bold pt-8 pb-2  ">
                 Beautiful Designs
               </h3>
@@ -91,7 +98,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 justify-self-start py-1">Figma</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className="text-center shadow-2xl p-10 rounded-xl my-10 dark:bg-white flex-1">
               <h3 className="text-lg font-bold pt-8 pb-2 ">
                 Code your dream project
               </h3>
@@ -101,7 +108,7 @@ export default function Home() {
               </p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className="text-center shadow-2xl p-10 rounded-xl my-10 dark:bg-white flex-1">
               <h3 className="text-lg font-bold pt-8 pb-2 ">Consulting</h3>
               <p className="py-2">
                 Are you interested in feedback for your current project? I can
@@ -125,6 +132,9 @@ export default function Home() {
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               I offer from a wide range of services, including brand design,
               programming and teaching.
+            </p>
+            <p class=" text-center text-white pt-36 font-mono text-lg mb-10">
+              "Copyright ©2023 All rights reserved | This Website is made by Mahlet Demeke "
             </p>
           </div>
         </section>
